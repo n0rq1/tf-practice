5ff2506e675bc1797002834b405453575d916d7c
Austin Norquist <austin.norquist@liatrio.com>
2025-09-08 15:18:38 -0700

    dasfsafaf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
-  value       = aws_instance.new_server.private_dns
+  value       = module.ec2_instance.private_dns
 }
