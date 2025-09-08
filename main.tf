36ed10e26bfce43d55117e7b7e52ae486d2abe55
Austin Norquist <austin.norquist@liatrio.com>
2025-09-08 09:14:48 -0700

    testing

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
-  enable_dns_hostnames    = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_instance" "app_server" {
